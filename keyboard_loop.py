297f1683883e2e02ca9a7788d7224902166044fe
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-10-02 23:52:11 -0400

    added vals

--- keyboard_loop.py
+++ keyboard_loop.py
@@ -5,11 +5,16 @@ values = {1 : "a", 2 : "d", 3 : "w", 4: "s"}
 
 
 
+
+def angle_gen(length):
+    angle = 360 / length
+    return(angle)
+
 while True:
     for key, value in values.items():
         if keyboard.is_pressed(value):
-            print(value, "is pressed")
-    
+            mult = angle_gen(len(values))
+            print(key * mult, "is pressed")
     if keyboard.read_key() == "p":
         print("You pressed p")
         break
