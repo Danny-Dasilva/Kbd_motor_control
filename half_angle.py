383a05a728382e59fa8ac26ba8f437ce5e72fbf2
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-10-05 00:33:56 -0400

    added pseudo recursion for half angle

---
+++ half_angle.py
@@ -0,0 +1,53 @@
+##from step.API import GrblComm
+
+import keyboard
+
+class Step:
+
+    def __init__(self):
+        self.angle = 4
+    def set_angle(self, input_ang):
+        if input_ang:
+            self.angle = 4 + (input_ang/360)
+        #self.comm.rapidMovement("X" + str(self.angle) + " F4")
+        print(str(self.angle))
+
+
+current_degree = 0
+def change(input_ang):
+    global current_degree
+    if current_degree != input_ang:
+        change = 0 
+        diff = (current_degree - input_ang)%360
+        if diff < 0:
+            change = 1
+        else:
+            change = -1
+        if abs(diff) > 180:
+            change = 0 - change
+        current_degree += change
+        print(current_degree)
+
+
+
+values = {1 : "q", 2 : "w", 3 : "e", 4: "r"}
+
+
+
+def angle_gen(length):
+    angle = 360 / length
+    return(angle)
+step = Step()
+while True:
+    
+    for key, value in values.items():
+        if keyboard.is_pressed(value):
+            
+            mult = angle_gen(len(values))
+            angle = key * mult
+            print(angle, "is pressed")
+            #step.set_angle(angle)
+            change(angle)
+    if keyboard.read_key() == "p":
+        print("You pressed p")
+        break
